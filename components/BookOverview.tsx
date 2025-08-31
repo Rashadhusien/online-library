2217651f4d1da86f3300a954730b1fc51f10113d
Rashadhusien <rashadhusein440@gmail.com>
2025-08-31 14:06:27 +0300

    fix bookovervieo check

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -30,10 +30,8 @@ const BookOverview = async ({
     .where(eq(users.id, userId))
     .limit(1);
 
-  if (!user) return null;
-
   const borrowingEligibility = {
-    isEligible: availableCopies > 0 && user.status === "APPROVED",
+    isEligible: availableCopies > 0 && user?.status === "APPROVED",
     message:
       availableCopies <= 0
         ? "Book is not available"
@@ -67,11 +65,13 @@ const BookOverview = async ({
         </div>
         <p className="book-description">{description}</p>
 
-        <BorrowBook
-          bookId={id}
-          userId={userId}
-          borrwoingEligibility={borrowingEligibility}
-        />
+        {user && (
+          <BorrowBook
+            bookId={id}
+            userId={userId}
+            borrwoingEligibility={borrowingEligibility}
+          />
+        )}
       </div>
 
       <div className="relative flex flex-1 justify-center">
